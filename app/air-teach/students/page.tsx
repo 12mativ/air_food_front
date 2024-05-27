c3b69c285c2ecd140457d16bc64f178f5ce3680c
12mativ <ivan_m146@mail.ru>
2024-05-28 01:05:43 +0400

    fixes

--- app/air-teach/students/page.tsx
+++ app/air-teach/students/page.tsx
@@ -1,17 +1,15 @@
-'use client'
+"use client";
 
-import axios from "axios";
-import React, { ChangeEvent, useEffect, useState } from "react";
-import { IoIosSearch } from "react-icons/io";
+import StudentCard from "@/components/StudentCard";
+import { Button } from "@/components/ui/button";
 import { useAppDispatch, useAppSelector } from "@/hooks/redux-hooks";
 import { getStudents } from "@/http/students/studentsAPI";
 import { addStudents, IStudent } from "@/lib/features/students/studentsSlice";
-import { makeAuth } from "@/lib/features/user/userSlice";
-import { LogOut } from "lucide-react";
+import { isAdmin, isStudent } from "@/utils/roles";
+import Link from "next/link";
 import { redirect } from "next/navigation";
-import StudentCard from "@/components/StudentCard";
-import { LiaUserCircleSolid } from "react-icons/lia";
-import { isStudent } from "@/utils/roles";
+import { ChangeEvent, useEffect, useState } from "react";
+import { IoIosSearch } from "react-icons/io";
 
 const Page = () => {
   const [studentForSearch, setStudentForSearch] = useState("");
@@ -171,30 +169,53 @@ const Page = () => {
     return pageButtons;
   };
 
-    if (isStudent(user)) {
-        return redirect('/air-teach/lk')
-    }
-    
+  if (isStudent(user)) {
+    return redirect("/air-teach/lk");
+  }
 
-    return (
-        <div className="w-full h-full items-center p-5">
-            <div>
-                <form className="hover:text-sky-600">
-                    <input className="shadow-lg rounded-xl w-full md:w-[40%] lg:h-10 xl:w-[28%] 2xl:w-[20%] h-12 pl-4 md:h-8" type="text" onChange={(e) => handleInputChange(e)} placeholder="Поиск студента" />
-                </form>
-            </div>
-            <div className="flex flex-wrap mx-10 pt-32 pb-20">
-                {students.map((student: IStudent) => (
-                    <StudentCard key={student.id} student={student} />
-                ))}
-            </div>
-            <div className="fixed bottom-0 left-0 w-full p-4 shadow-lg bg-[#ebebeb]">
-                <div className="flex justify-center">
-                    {renderPageButtons()}
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full h-full items-center bg-[#ebebeb]">
+      {isAdmin(user) && (
+        <Link href={"/air-teach/students"}>
+          <Button className="ml-12 w-32 bg-[#7f7f7f] text-white hover:bg-sky-500">
+            Все студенты
+          </Button>
+        </Link>
+      )}
+      {isAdmin(user) && (
+        <Link href={"/air-teach/courses"}>
+          <Button className="ml-12 w-32 bg-[#cecece] text-[#7f7f7f] hover:bg-sky-500 hover:text-white">
+            Все курсы
+          </Button>
+        </Link>
+      )}
+      <div className="mt-4 mx-10 relative">
+        <input
+          className="shadow-lg rounded-xl w-full md:w-[40%] lg:h-10 xl:w-[28%] 2xl:w-[20%] h-12 pl-8 md:h-8"
+          type="text"
+          onChange={(e) => handleInputChange(e)}
+          placeholder="Поиск студента"
+        />
+        <IoIosSearch className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500" />
+      </div>
+      <div className="flex flex-wrap mx-10 pt-2 pb-20">
+        {students.length > 0 ? (
+          students.map((student: IStudent) => (
+            <StudentCard key={student.id} student={student} />
+          ))
+        ) : (
+          <div className="flex items-center justify-center h-full w-full">
+            <p className="text-center text-gray-500 text-lg">
+              Студенты не найдены
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="fixed bottom-0 left-0 w-full p-4 shadow-lg bg-[#ebebeb]">
+        <div className="flex justify-center">{renderPageButtons()}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
