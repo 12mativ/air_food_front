ec775311309f3459e05be6289b3d1b2397879627
A1Fizzy <artemarhunin@gmail.com>
2024-06-03 16:18:08 +0400

    fix deleting courses

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -6,9 +6,12 @@ import { formateComplexDate } from "@/utils/formateComplexDate";
 import { X } from "lucide-react";
 import { useModal } from "@/hooks/use-modal-store";
 import { DeleteCourseModal } from "./modals/delete-course-modal";
+import { isAdmin, isCourseOrganiser } from "@/utils/roles";
+import { useAppSelector } from "@/hooks/redux-hooks";
 
 const CourseCard: React.FC<{ course: ICourse }> = ({ course }) => {
   const { onOpen } = useModal();
+  const user = useAppSelector((state) => state.userReducer.user);
 
   const handleDelete = (e: React.MouseEvent<HTMLDivElement | SVGSVGElement>) => {
     e.preventDefault();
@@ -22,12 +25,14 @@ const CourseCard: React.FC<{ course: ICourse }> = ({ course }) => {
         href={`/air-teach/courses/${course.id}`}
         className="relative hover:scale-[1.01] transition bg-white rounded-xl p-4 shadow-md m-2 h-40 w-full md:w-[47%] lg:w-[30%] xl:w-[23.5%] 2xl:w-[18.5%]"
       >
-        <div onClick={handleDelete}>
-          <X
-            size={20}
-            className="absolute right-2 text-red-500 hover:scale-[1.1] transition cursor-pointer"
-          />
-        </div>
+        {(isCourseOrganiser(user) || isAdmin(user)) && (
+          <div onClick={handleDelete}>
+            <X
+              size={20}
+              className="absolute right-2 text-red-500 hover:scale-[1.1] transition cursor-pointer"
+            />
+          </div>
+        )}
         <p className="text-l font-semibold">{course.name}</p>
         <p className="text-sm">
           {formateComplexDate(course.startDate) || ""} -{" "}
