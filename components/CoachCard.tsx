6947a31d71b5372bb5aef9b0f5fd59b8f702b8f3
A1Fizzy <artemarhunin@gmail.com>
2024-07-04 13:54:01 +0400

    fix coachCard responsive

--- components/CoachCard.tsx
+++ components/CoachCard.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const CoachCard: React.FC<{ coach: ICoach }> = ({ coach }) => (
   <Link
     href={`/air-teach/coaches/${coach.id}`}
-    className="hover:scale-[1.01] transition bg-white rounded-xl p-4 shadow-md m-2 h-40 w-fit"
+    className="relative hover:scale-[1.01] transition bg-white rounded-xl p-4 shadow-md m-2 h-40 w-full md:w-[47%] lg:w-[30%] xl:w-[23.5%] 2xl:w-[18.5%]"
   >
     <p className="text-l font-semibold">{coach.email}</p>
     <p className="text-sm">
