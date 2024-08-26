8dd99412833eda1049323ae7da98da0bac28c485
12mativ <ivan_m146@mail.ru>
2024-08-27 00:07:55 +0400

    refactor: add new useAppDispatch useAppSelector useAppStore declarations

--- hooks/redux-hooks.ts
+++ hooks/redux-hooks.ts
@@ -1,8 +1,17 @@
-import { AppDispatch, AppStore, RootState } from '@/lib/store'
 import { useDispatch, useSelector, useStore } from 'react-redux'
-import type { TypedUseSelectorHook } from 'react-redux'
+import type { RootState, AppDispatch, AppStore } from '@/lib/store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-export const useAppStore: () => AppStore = useStore
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppStore = useStore.withTypes<AppStore>()
+
+
+// import { AppDispatch, AppStore, RootState } from '@/lib/store'
+// import { useDispatch, useSelector, useStore } from 'react-redux'
+// import type { TypedUseSelectorHook } from 'react-redux'
+
+// // Use throughout your app instead of plain `useDispatch` and `useSelector`
+// export const useAppDispatch: () => AppDispatch = useDispatch
+// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+// export const useAppStore: () => AppStore = useStore
